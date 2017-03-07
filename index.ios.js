ccef2d534c3931dc5af0f6d7c8c503a6969019e2
thanhcs94 <nguyenvanthanh9294@gmail.com>
2017-03-07 12:45:51 +0700

    image tag

--- index.ios.js
+++ index.ios.js
@@ -9,21 +9,22 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
 export default class LearnReactnative extends Component {
   render() {
     return (
       <View style={styles.container}>
-
-      <View style = {styles.content}>
-        <Text>
-        Thanks all , i love all u guy
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <Text style={styles.welcome}>
+          Who is the most handsome man in the world?
         </Text>
-      </View>
-
+        <Image
+        style={styles.img}
+        //source={{uri: 'https://pbs.twimg.com/profile_images/806966317093785601/WLB8r27j.jpg'}}
+        source={require('./img/thanhcs94.jpg')}
+        />
       </View>
     );
   }
@@ -36,10 +37,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  content:{
-    backgroundColor: "#ccc",
-    flex :1
-  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -50,6 +47,11 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  img:{
+    width: 70,
+    height: 70,
+  },
 });
+
 
 AppRegistry.registerComponent('LearnReactnative', () => LearnReactnative);
